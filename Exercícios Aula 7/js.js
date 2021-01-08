b0f7a0d9a324b03a599cbe732419bce689035c75
Fernando Borba <fsborba5@gmail.com>
2021-01-08 15:44:01 -0300

    exercícios aula 7

---
+++ Exercícios Aula 7/js.js
@@ -0,0 +1,43 @@
+// EXERCÍCIOS - 1
+
+var posit = 10
+console.log(`${posit} é um número positivo!`)
+
+var neg = -15
+console.log(`${neg} é um número negativo!`)
+
+var dec = 12.3
+console.log(`${dec} é um número decimal!`)
+
+var nome = 'Fernando e R$ 1.000,00'
+console.log(`${nome} são string!`)
+
+var dia = '11'
+if(dia != 10) {
+console.log('Hoje não é dia de din din :( ')
+} else {
+console.log('Hoje é dia de din din :)')}
+
+var nome = 'Fernando'
+var nome1 = 'Sant Anna'
+var nome2 = 'Borba'
+console.log(`Meu nome completo é ${`${nome} ${nome1} ${nome2}`}`)
+
+var nome = "Clara"; //retorna o nome
+console.log(nome)
+
+var nome ="Clara";
+console.log(nome, sobrenome); // retorna erro, pois, o sobrenome não está definido
+
+var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+console.log(texto)
+
+var valorNulo = 'null'
+var naoDefinido = 'undefined'
+console.log(valorNulo + ' e ' + naoDefinido)
+
+var frutas = 'abacaxi, melão, uva, banana e melancia'
+console.log('Estamos na estação de: ' + frutas + ', OBA!!')
+
+var digitalHouse = 'Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam!'
+console.log(digitalHouse)
