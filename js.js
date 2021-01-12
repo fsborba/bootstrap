8b1b8fcde444794d7e0dbfd2c1a779c3d8b56c18
Fernando Borba <fsborba5@gmail.com>
2021-01-12 19:00:41 -0300

    aula

---
+++ js.js
@@ -0,0 +1,7 @@
+var n = 61
+
+if (n % 2 == 0) {
+    alert("O número é par, pois o resto da divisão por dois é igual a zero")
+        }else{
+    alert("Existe resto na divisão por dois, portando o número i é ímpar")
+}
