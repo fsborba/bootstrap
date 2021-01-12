2fca2d6efa37f75de136bb77c95030ae2c168b93
Fernando Borba <fsborba5@gmail.com>
2021-01-12 20:14:18 -0300

    aua

---
+++ AULA_2021/script.js
@@ -0,0 +1,10 @@
+function somar(numero1, numero2){
+
+    var resultado = numero1 + numero2;
+
+    console.log("O resultado da soma é: "+resultado);
+}
+
+function boasVindas() [
+    alert("Seja bem vindo, vocêpressionou o btão cinza!")
+]
