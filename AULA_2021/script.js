655ca92265e7d8651cf10fda572b9cc56ed5f772
Fernando Borba <fsborba5@gmail.com>
2021-01-14 22:29:28 -0300

    final de aula

--- AULA_2021/script.js
+++ AULA_2021/script.js
@@ -1,11 +1,53 @@
-function somar(numero1, numero2){
-
-    var resultado = numero1 + numero2;
-
-    console.log("O resultado da soma é: "+resultado);
+function limparParcial(){
+    document.getElementById('num1').value = "";
+    document.getElementById('num2').value = "";
+    document.getElementById('operacao').value = "";
 }
 
-function boasVindas() {
-    alert("Seja bem vindo, você pressionou o botão cinza!")
-
+function limpar(){
+    document.getElementById('num1').value = "";
+    document.getElementById('num2').value = "";
+    document.getElementById('operacao').value = "";
+    document.getElementById('resultado').value = "";
 }
+
+if(numero1 == ''){
+    alert('Favor digitar um número válido para operação (num1)');
+    return false;
+}
+
+if(numero1 == ''){
+    alert('Favor digitar um número válido para operação (num2');
+    return false; 
+}
+
+function calcular() {
+    var numero1 = document.getElementById('num1').value;
+    var numero2 = document.getElementById('num2').value;
+    var operacao = document.getElementById('operacao').value;
+    var resultado;
+
+    numero1 = Number(numero1);
+    numero2 = Number(numero2);
+
+    switch(operacao){
+        case 'sub': resultado = numero1 - numero2;
+        break;
+        case 'adi': resultado = numero1 + numero2;
+        break;
+        case 'mult': resultado = numero1 * numero2;
+        break;
+        case 'div':
+            if(num2 == 0){
+                alert('Não divisível por 0')
+                return.false;
+            } resultado =
+            break;
+        default: alert ("escolha uma operação válida");
+        return false;
+
+    alert(resultado);
+
+    limparParcial();
+    document.getElementById('resultado').value = resultado;
+    }
