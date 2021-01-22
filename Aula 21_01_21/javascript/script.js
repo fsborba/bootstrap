1125e4fbfca19dcd2d1b4a39b200088208f2ac54
Fernando Borba <fsborba5@gmail.com>
2021-01-21 22:40:11 -0300

    ang

---
+++ Aula 21_01_21/javascript/script.js
@@ -0,0 +1,26 @@
+function envio() {
+    var nome = $("#nome").val();
+    var cidade = $("#cidade").val();
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/usuarios",
+        data: {
+            "id": 3,
+            "nome": nome,
+            "cidade": cidade
+        },
+        success: function () {
+            alert('Dados salvos com sucesso!')
+        },
+        error: function () {
+            alert('Ocorreu um erro inesperado!')
+        }
+    });
+}
+/*.ajax({
+    method: "POST",
+    url: "http://localhost:3000/usuarios",
+    data: {
+        "id"
+    }
+})
